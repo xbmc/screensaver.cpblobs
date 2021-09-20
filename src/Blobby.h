f977756cd762546413dcaeb60db7912e1f4c9f12
Alwin Esch <alwin.esch@web.de>
2021-09-20 06:53:06 +0200

    update copyright year to 2021

--- src/Blobby.h
+++ src/Blobby.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2005-2020 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2005-2021 Team Kodi (https://kodi.tv)
  *  Copyright (C) Simon Windmill (siw@coolpowers.com)
  *  Ported to Kodi GL4 by Alwin Esch <alwinus@kodi.tv>
  *
